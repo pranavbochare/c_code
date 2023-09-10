b66e7d578e71f9c847fa456a2e1a85e6d097ae30
PranaV_B <144541695+pranavbochare@users.noreply.github.com>
2023-09-10 15:51:38 +0530

    Delete for_loop directory

--- for_loop/19_table.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    for(int i=19;i<=190;i=i+19){
-        printf("%d\n",i);
-    }
-    return 0;
-}
--- for_loop/ascii.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    for(int i=65;i<=90;i++){
-        char ch=(char)i;
-        printf("%c\n",ch);
-    }
-    return 0;
-}
